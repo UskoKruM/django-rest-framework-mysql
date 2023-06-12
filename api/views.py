653872a0913f66b6e7ea5482dab8861391d8c453
UskoKruM <o.Oscar.-@hotmail.com>
2023-06-11 22:13:01 -0500

    Commit 001: Primera carga a GitHub.

---
+++ api/views.py
@@ -0,0 +1,10 @@
+from rest_framework import viewsets
+from .models import Company
+from .serializer import CompanySerializer
+
+# Create your views here.
+
+
+class CompanyViewSet(viewsets.ModelViewSet):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
